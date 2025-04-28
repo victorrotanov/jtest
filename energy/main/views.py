a69eb170802e0894d5468228f833f833637bc7c0
Victor Rotanov <rvvtux@gmail.com>
2025-04-28 21:59:11 +0500

    vacancy isActive added

--- energy/main/views.py
+++ energy/main/views.py
@@ -128,7 +128,7 @@ def ourprofs(request):
 
 def vacancy(request):
     context = get_default_context()
-    context['info'] = Vacancy.objects.all()
+    context['info'] = Vacancy.objects.filter(isActive=True)
     return render(request, 'main/vacancy.html', context)
 
 def test(request):
