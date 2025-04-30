e6ffa0ed8f57aac7026fca46d42daabfff3f8bc6
Victor Rotanov <rvvtux@gmail.com>
2025-04-30 13:27:58 +0500

    повторный комит

--- energy/main/models.py
+++ energy/main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class News(models.Model):
 
     title = models.CharField(max_length=200, verbose_name="Заголовок")
+    anonce = models.TextField(verbose_name="Анонс")
     content = models.TextField(verbose_name="Содержание")
     created_at = models.DateTimeField(
         auto_now_add=True, verbose_name="Дата создания")
